4fd0baf4473b18f49612f9e664dcd0e2a1270536
Ahmed Elatrouz <ahmed.elatrouz@orange.com>
2024-03-08 20:50:04 +0100

    [fraud-service] : configure fraud microservice

---
+++ fraud-service/src/main/java/com/aelatrouz/fraud/FraudApplication.java
@@ -0,0 +1,12 @@
+package com.aelatrouz.fraud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FraudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FraudApplication.class);
+	}
+}
